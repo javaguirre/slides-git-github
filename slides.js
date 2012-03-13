11f8b4bfa01581d8793729ca793e69d55059ce7e
Miguel de la Cruz Fernández <mgdelacroix@gmail.com>
2012-03-13 17:17:58 +0100

    Última versión antes de la charla?? :P

--- slides.js
+++ slides.js
@@ -10,7 +10,8 @@
   URL: http://code.google.com/p/html5slides/
 */
 
-var PERMANENT_URL_PREFIX = 'http://localhost/~javaguirre/api-js-guluc3m/';
+//var PERMANENT_URL_PREFIX = 'http://localhost/~javaguirre/api-js-guluc3m/';
+var PERMANENT_URL_PREFIX = 'http://localhost:8000/';
 
 var SLIDE_CLASSES = ['far-past', 'past', 'current', 'next', 'far-next'];
 
